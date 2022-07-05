f6a1e9a475d93aaf3bb836c5bd7ad2dc26241d2e
emvasquezc <emvasquezc@unal.edu.co>
2022-07-04 21:01:22 -0500

    Avance increible

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -15,71 +15,76 @@ En luigi llame las funciones que ya creo.
 import luigi
 from luigi import Task
 
-class luigi_ingest_data(Task):
 
+class luigi_ingest_data(Task):
     def output(self):
-        return luigi.LocalTarget('data_lake/landing/result.txt')
+        return luigi.LocalTarget("data_lake/landing/result.txt")
 
     def run(self):
         from ingest_data import ingest_data
-        with self.output().open('w') as files_ingested:
+
+        with self.output().open("w") as files_ingested:
             ingest_data()
 
-class luigi_transform_data(Task):
 
+class luigi_transform_data(Task):
     def requires(self):
         return luigi_ingest_data()
 
     def output(self):
-        return luigi.LocalTarget('data_lake/raw/results2.txt')
+        return luigi.LocalTarget("data_lake/raw/result.txt")
 
     def run(self):
         from transform_data import transform_data
-        with self.output().open('w') as files_transformed:
+
+        with self.output().open("w") as files_transformed:
             transform_data()
 
-class luigi_clean_data(Task):
 
+class luigi_clean_data(Task):
     def requires(self):
         return luigi_transform_data()
 
     def output(self):
-        return luigi.LocalTarget('data_lake/cleansed/results3.txt')
+        return luigi.LocalTarget("data_lake/cleansed/result.txt")
 
     def run(self):
         from clean_data import clean_data
-        with self.output().open('w') as f:
+
+        with self.output().open("w") as f:
             clean_data()
 
-class luigi_compute_daily_prices(Task):
 
+class luigi_compute_daily_prices(Task):
     def requires(self):
         return luigi_clean_data()
 
     def output(self):
-        return luigi.LocalTarget('data_lake/business/results4.csv')
+        return luigi.LocalTarget("data_lake/business/result.txt")
 
     def run(self):
         from compute_daily_prices import compute_daily_prices
-        with self.output().open('w') as f:
+
+        with self.output().open("w") as f:
             compute_daily_prices()
 
-class luigi_compute_monthly_prices(Task):
 
+class luigi_compute_monthly_prices(Task):
     def requires(self):
         return luigi_compute_daily_prices()
 
     def output(self):
-        return luigi.LocalTarget('data_lake/business/results5.csv')
+        return luigi.LocalTarget("data_lake/business/result.txt")
 
     def run(self):
         from compute_monthly_prices import compute_monthly_prices
-        with self.output().open('w') as f:
+
+        with self.output().open("w") as f:
             compute_monthly_prices()
 
 
 if __name__ == "__main__":
-    
+
     luigi.run(["luigi_compute_monthly_prices", "--local-scheduler"])
 
     # raise NotImplementedError("Implementar esta función")
--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -17,25 +17,33 @@ def transform_data():
 
     import glob
     import pandas as pd
-    archivos = glob.glob('data_lake/landing/*xls*')
-    usecols = ['Fecha'] + [str(i) for i in range(24)]
-    dtype = {'Fecha': str}
-    
+
+    archivos = glob.glob("data_lake/landing/*xls*")
+    usecols = ["Fecha"] + [str(i) for i in range(24)]
+    dtype = {"Fecha": str}
+
     for archivo in archivos:
         df = pd.read_excel(archivo, dtype=dtype)
         # Eliminar las filas que no tienen datos
-        if df.columns[0] != 'Fecha':
-            indice_inicio_df = int(df[df.iloc[:, 0] == 'Fecha'].index.values) + 1
-            df = pd.read_excel(archivo, usecols=usecols, 
-                                skiprows=indice_inicio_df, dtype=dtype)
+        if df.columns[0] != "Fecha":
+            indice_inicio_df = int(df[df.iloc[:, 0] == "Fecha"].index.values) + 1
+            df = pd.read_excel(
+                archivo, usecols=usecols, skiprows=indice_inicio_df, dtype=dtype
+            )
         # Quitar 00:00:00 de las fechas
-        df['Fecha'] = df['Fecha'].astype(str).apply(lambda x: x.split(' ')[0])
-        name_file = archivo.rsplit('/')[-1]
-        df.to_csv('data_lake/raw/' + name_file.split('.')[0] + '.csv', index=False)   
+        df["Fecha"] = df["Fecha"].astype(str).apply(lambda x: x.split(" ")[0])
+        name_file = archivo.rsplit("/")[-1]
+        df.to_csv("data_lake/raw/" + name_file.split(".")[0] + ".csv", index=False)
 
+
+def test_transform_data():
+    import os
+
+    assert os.path.isfile("data_lake/raw/1995.csv") is True
 
 
 if __name__ == "__main__":
+
     transform_data()
     import doctest
 
